4ca2444006921cffadc0155b5097e324cbe77d12
JorgeSazo <jorge.sazo8956@gmail.com>
2026-09-01 11:44:18 -0600

    envio segunda parte clase controlador

--- Controlador.py
+++ Controlador.py
@@ -1,4 +1,5 @@
-
+import os
+# aca se importaran las otras clases, creo
 class controlador:
     def __init__(self, ventana_principal):
         """
@@ -8,6 +9,19 @@ class controlador:
         self.ventana = ventana_principal
         self.vantana.title("Sistema de Medición de Vibraciones") 
         self.ventana.geometry("500x400")
-
         self.ruta_archivo = None
-    
+        #atributo que guarda la ruta del archivo
+
+        # --- Componentes de la Interfaz --- 
+
+        self.label.titulo = tk.Label(
+            self.ventana,
+            text="Medidor de Vibraciones Vehiculares",
+            font=("Arial", 16, "bold")
+        )
+        self.label_titulo.pack(pady=15)
+        #boton de interaccion 
+        self.boton_cargar = tk.boton(
+            self.ventana, 
+            text="1. Cargar Archivo",
+        )
